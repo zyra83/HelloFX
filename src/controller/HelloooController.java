e4209adc224dc39a94341727835100947b0eb4d5
Mickaël Benoit <zyra83@hotmail.com>
2017-05-02 13:23:23 +0200

    Ajout d'un radio group

--- src/controller/HelloooController.java
+++ src/controller/HelloooController.java
@@ -2,22 +2,32 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import lombok.extern.apachecommons.CommonsLog;
 
 @CommonsLog
 public class HelloooController {
+	// TODO ajouter un alvel et y afficher le resultat
 
 	@FXML
 	ComboBox<String> maCombo;
 
+	@FXML
+	private ToggleGroup tgGlacon;
+	
+	@FXML
+	private RadioButton rbAvec, rbSans;
+	
+	
 	public HelloooController() {
 
 	}
 
 	public void cliquer() {
 		if (log.isInfoEnabled()) {
-			log.info(String.format("Clique sur le bouton : %s", maCombo.getValue()));
 			log.info(String.format("Clique sur le bouton : %s", maCombo.getSelectionModel().getSelectedItem()));
+			log.info(String.format("Selected toggle : %s", tgGlacon.getSelectedToggle().getUserData()));
 		}
 
 	}
@@ -28,6 +38,8 @@ public class HelloooController {
 	@FXML
 	public void initialize() {
 		maCombo.getItems().addAll("a", "b", "c");
+		rbAvec.setUserData(true);
+		rbSans.setUserData(false);
 	}
 
 }
